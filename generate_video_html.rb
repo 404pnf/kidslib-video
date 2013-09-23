e47e8bae750ee66a29f18e473b3cdeeb8e475951
404pnf <404pnf@exmaple.org>
2013-09-23 17:04:31 +0800

    规范化文件夹名称

--- generate_video_html.rb
+++ generate_video_html.rb
@@ -1,12 +1,14 @@
 require 'csv'
 require 'erubis'
+require 'FileUtils'
 
 def generate_video_html(path)
 	CSV.read(path).each do |id, title, *_|
+    id.strip! # 有些id的文本前面有空白！
     page_title = title
-    flv_url = "flv/#{ id.strip }.flv"
+    flv_url = "flv/#{ id }.flv"
 
-    input = File.read('index.eruby')
+    input = File.read('views/index.eruby')
     eruby = Erubis::Eruby.new(input)    # create Eruby object
     index_html =  eruby.result(binding) # get result
 
@@ -15,8 +17,18 @@ def generate_video_html(path)
   end
 end
 
+def copy_asset_to_output
+  # If you want to copy all contents of a directory instead of the
+  # directory itself, c.f. src/x -> dest/x, src/y -> dest/y,
+  # use following code.
+  # cp_r('src', 'dest') makes dest/src,
+  # but this doesn't.
+  FileUtils.cp_r 'views/.', 'output', :verbose => true
+end
+
 if __FILE__ == $PROGRAM_NAME
-  path = ARGV[0]
+  path = ARGV[0] || 'csv/all_vid.csv'
   p "输入文件是#{ path }"
   generate_video_html(path)
+  copy_asset_to_output
 end
